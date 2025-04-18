2cf21920579dc38561ef570aa154f0e287b94a7e
Name <magsabir95@gmail.com>
2025-04-18 09:43:56 +0300

    update

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,14 +2,14 @@ import {userService} from "../domain/users.service";
 import {Request, Response} from "express";
 
 
-export const usersController = {
-    postController: async (req: Request, res: Response) => {
-        const user = await userService.createUserService(req.body)
-
-        res.status(200).send(user)
-    },
-    async getUser(req: Request, res: Response) {
-        const user = await userService.getUsers()
-        res.status(200).send(user)
-    }
-}
+// export const usersController = {
+//     postController: async (req: Request, res: Response) => {
+//         const user = await userService.createUserService(req.body)
+//
+//         res.status(200).send(user)
+//     },
+//     async getUser(req: Request, res: Response) {
+//         const user = await userService.getUsers()
+//         res.status(200).send(user)
+//     }
+// }
